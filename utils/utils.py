27b4c2a0793ef7bd06b84ce86867776fb9766dee
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-09-14 07:42:25 +0200

    Add get_pretrained_resnet and reduce dataset size

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,8 @@ from data.dataset import Dataset
 from torch.utils.data import DataLoader
 
 
-import torch
+import torch, torchvision
+import torch.nn as nn
 import numpy as np
 import os
 import random
@@ -22,10 +23,10 @@ def set_seed(seed):
 
 def get_dataloaders(root_path, batch_size, transform):
 
-    filenames = os.listdir(root_path) + os.listdir(root_path)
+    # get only 1000 samples
+    filenames = os.listdir(root_path)[:500] + os.listdir(root_path)[:-500:-1]
     labels = [1 if f.split(".")[0] == 'dog' else 0 for f in filenames]
 
-    # 1500 images in training, 5000 images in validation and 5000 images for testing
     x_trainval, x_test, y_trainval, y_test = train_test_split(filenames, labels, stratify=labels, test_size=0.2)
     x_train, x_val, y_train, y_val = train_test_split(x_trainval, y_trainval, stratify=y_trainval, test_size=0.25)
 
@@ -38,3 +39,19 @@ def get_dataloaders(root_path, batch_size, transform):
     test_loader = DataLoader(test_ds, batch_size=1, shuffle=False)
 
     return train_loader, val_loader, test_loader
+
+
+def get_pretrained_resnet():
+
+    model = torchvision.models.resnet18(pretrained=True)
+
+    # fine tune only the last fully connected layer
+
+    # get the number of input features
+    num_features = model.fc.in_features
+
+    # update the number of input features and the number
+    # of output features for our task (2: dog and cat, 1 and 0)
+    model.fc = nn.Linear(in_features=num_features, out_features=2)
+
+    return model
